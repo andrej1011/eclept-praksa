a8a92387aa26ae285a31612df3c81ebae428c8a3
andrej <Andrej@mba.local>
2026-05-21 00:17:47 +0200

    added showings CRUD

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from app.api.routes import auditorium,genre,movie
+from app.api.routes import auditorium,genre,movie,showing
 from fastapi import FastAPI
 
 from app.api.routes import auth
@@ -9,6 +9,7 @@ app.include_router(auth.router)
 app.include_router(auditorium.router)
 app.include_router(genre.router)
 app.include_router(movie.router)
+app.include_router(showing.router)
 
 @app.get("/")
 def index():
---
+++ app/services/showing.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException, status
+from sqlalchemy import func
+from sqlalchemy.orm import Session
+from uuid import UUID
+from datetime import date
+
+from app.models.showing import Showing
+from app.schemas.showing import ShowingCreate, ShowingUpdate
+
+class ShowingService:
+    def __init__(self, db: Session):
+        self._db = db
+
+    def get_all(self) -> list[Showing]:
+        return self._db.query(Showing).all()
+
+    def get_one(self, showing_id: UUID) -> Showing:
+        s = self._db.query(Showing).filter(Showing.id == showing_id).first()
+        if not s:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, "Showing not found")
+        return s
+
+    def create(self, data: ShowingCreate) -> Showing:
+        showing = Showing(
+            movie_id=data.movie_id,
+            auditorium_id=data.auditorium_id,
+            start_time=data.start_time,
+            booked_seats=0,
+        )
+        self._db.add(showing)
+        self._db.commit()
+        self._db.refresh(showing)
+        return showing
+
+    def update(self, showing_id: UUID, data: ShowingUpdate) -> Showing:
+        showing = self.get_one(showing_id)
+        for k, v in data.model_dump(exclude_unset=True).items():
+            setattr(showing, k, v)
+        self._db.commit()
+        self._db.refresh(showing)
+        return showing
+
+    def delete(self, showing_id: UUID) -> None:
+        showing = self.get_one(showing_id)
+        self._db.delete(showing)
+        self._db.commit()
