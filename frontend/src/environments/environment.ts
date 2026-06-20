82f60f38e42ac6e9c680e7899144b616d1abce54
andrej1011 <82315624+andrej1011@users.noreply.github.com>
2026-06-20 09:52:58 +0200

    Update API URL for production environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000', //change when uploaded to firebase
+  apiUrl: 'https://eclept-praksa-vwng.onrender.com/', //change when uploaded to firebase
 };
